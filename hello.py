b9f8b122eea76ede2bb194ab787a35e783d6aa4f
mdfaizaandev <m.faizaan.dev@gmail.com>
2026-05-29 14:19:08 +0530

    Day 1: Clean variables script baseline

---
+++ hello.py
@@ -0,0 +1,47 @@
+print ("Welcome")
+#Day 1 Variables and Data Type
+
+name = "Faizaan"
+age = 18
+height = 5.7
+print (name)
+print (age)
+print (5.7)
+
+print ("my name is: ", name)
+print ("my age is: ", age)
+
+# Arithmetic with variables
+birth_year = 2026 - age
+print(f"I was born in {birth_year}")
+
+# Type checking
+print(type (name))
+print(type (age))
+print(type (height))
+
+#String (str)
+name1 = 'ai'
+name2 = "ai"
+name3 = '''ai'''
+print (name1)
+print (name2)
+print (name3)
+
+#boolean and String
+age = 18
+old = False
+a = None
+print (type(old))
+print (type(a))
+
+#Program Print sum
+a = 7
+b = 2
+sum = a+b
+print(sum)
+#Program Print diff
+a = 2000
+b = 1000
+diff = a-b
+print(diff)
